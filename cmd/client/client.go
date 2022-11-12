ea7d6294cf78e6646710e0252116eaa3097bf606
John Wood <j@jdtw.us>
2022-11-11 21:50:12 -0800

    Remove keybase chatbot

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -10,21 +10,19 @@ import (
 
 	"jdtw.dev/links/pkg/client"
 	"jdtw.dev/links/pkg/frontend"
-	"jdtw.dev/links/pkg/keybase"
 	"jdtw.dev/links/pkg/links"
 	"jdtw.dev/token"
 )
 
 var (
-	priv       = flag.String("priv", "", "Path to private key; can also be specified via the LINKS_PRIVATE_KEY environment variable.")
-	addr       = flag.String("addr", "", "Appliction URI; can also be specified via the LINKS_ADDR environment variable")
-	index      = flag.String("index", "", "Set the root redirect")
-	add        = flag.String("add", "", "Add a redirect")
-	link       = flag.String("link", "", "The redirect")
-	get        = flag.String("get", "", "Get a redirect")
-	rm         = flag.String("rm", "", "Remove a redirect")
-	server     = flag.Int("server", -1, "If not -1, starts starts a frontent HTTP server on the given port.")
-	keybaseLoc = flag.String("keybase_loc", "", "If not empty, starts a keybase chat bot using the given keybase binary location.")
+	priv   = flag.String("priv", "", "Path to private key; can also be specified via the LINKS_PRIVATE_KEY environment variable.")
+	addr   = flag.String("addr", "", "Appliction URI; can also be specified via the LINKS_ADDR environment variable")
+	index  = flag.String("index", "", "Set the root redirect")
+	add    = flag.String("add", "", "Add a redirect")
+	link   = flag.String("link", "", "The redirect")
+	get    = flag.String("get", "", "Get a redirect")
+	rm     = flag.String("rm", "", "Remove a redirect")
+	server = flag.Int("server", -1, "If not -1, starts starts a frontent HTTP server on the given port.")
 )
 
 func main() {
@@ -58,8 +56,6 @@ func main() {
 		addr := fmt.Sprint(":", *server)
 		log.Printf("listening on %q", addr)
 		log.Fatal(http.ListenAndServe(addr, frontend.NewHandler(c)))
-	case *keybaseLoc != "":
-		log.Fatal(keybase.ChatBot(*keybaseLoc, c))
 	case *index != "":
 		if err := c.Put(links.Index, *index); err != nil {
 			log.Fatal(err)
--- go.mod
+++ go.mod
@@ -4,7 +4,6 @@ go 1.18
 
 require (
 	github.com/gorilla/mux v1.8.0
-	github.com/keybase/go-keybase-chat-bot v0.0.0-20221010143313-3c4907bab5dd
 	google.golang.org/protobuf v1.28.1
 	jdtw.dev/token v0.1.4
 )
@@ -20,14 +19,10 @@ require (
 	github.com/jackc/puddle v1.3.0 // indirect
 	golang.org/x/crypto v0.2.0 // indirect
 	golang.org/x/text v0.4.0 // indirect
-	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
 
 require (
-	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/jackc/pgx/v4 v4.17.2
-	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e
 	github.com/stretchr/testify v1.8.1 // indirect
-	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
